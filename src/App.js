77245bc3409b9365cc8e9fdaf7e51870f441d6a5
ReemaGowda <reemagowdan@gmail.com>
2019-05-14 16:31:07 -0400

    made changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import QRAddress21 from './components/QRAddress21';
+ import QRAddress21 from './components/QRAddress21';
+import Cashier from './components/Cashier';
 
 
 function App() {
   return (
     <div className="App">
       <QRAddress21 />
+      {/* <Cashier/> */}
     </div>
   );
 }
--- src/components/QRAddress21.js
+++ src/components/QRAddress21.js
@@ -5,29 +5,68 @@ export default class QRAddress21 extends React.Component {
     super(props);
     this.state = {
         value: '',
+         cryptoAmount: '0'
+        
       }
   }
 
   onValueChange = (e) => {
     this.setState({
       value: e.target.value,
+      cryptoAmount : this.state.cryptoAmount
     });
   }
 
   render() {
-    const { value } = this.props;
+    // const { value } = this.props;
+
+    // let options = {
+    //         amount: this.state.cryptoAmount,
+    //         label: '#BitcoinBay',
+    //       };
+    //      let XPubAddress = BITBOX.Address.fromXPub(XPubKey, "0/0");
+    //      let payQRAddress21 = BITBOX.BitcoinCash.encodeBIP21(XPubAddress, options);
+      
 
     return(
       <div className="qrcode wrapper">
       {/* <QRCode value="mtXWDB6k5yC5v7TcwKZHB89SUp85yCKshy"/> */}
-        <p>{ value }</p>
+        {/* <p>{ value }</p> */}
         <h1>QR CODE GENERATOR</h1>
-            <QRCode value={this.state.value}  />
+            <QRCode value={this.state.value} color="#fff" />
+          
             <br></br>
             <div>
               <input type="text" value={this.state.value} onChange={this.onValueChange}  />
+            </div>
+            <div>
+           <h1>Amount={this.state.value}  </h1>
             </div>
       </div>
     );
   }
 };
+
+
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import QRCode from 'qrcode-react';
+
+// export default class QRAddress21 extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     const { value } = this.props;
+
+//     return(
+//       <div>
+//         <QRCode value={ value } />
+//         <p>{ value }</p>
+//       </div>
+//     );
+//   }
+// };
+
