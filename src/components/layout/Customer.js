ac89bb2aa1f61ca8fa603fdf3677e465e0caf316
lightswarm124 <lightswarm@gmail.com>
2019-05-15 16:57:25 -0400

    added starting point for cashier and customer pages with login auth

--- src/components/layout/Customer.js
+++ src/components/layout/Customer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import axios from 'axios';
 import openSocket from 'socket.io-client';
 
 //import * as BITBOXCli from "bitbox-sdk";
@@ -9,15 +8,41 @@ import openSocket from 'socket.io-client';
 //const BITBOX = new BITBOXCli.default({ restURL: "https://trest.bitcoin.com/v2/" });
 
 const socket = openSocket('http://localhost:3000');
+const defaultWebURL = 'https://www.meetup.com/The-Bitcoin-Bay';
 
 export default class Customer extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.update = this.update.bind(this);
+    this.state = {
+      cryptoType: 'BCH',
+      fiatType: 'CAD',
+      cryptoAmount: 0,
+      fiatAmount: 0,
+      cryptoPrice: 0,
+      url: defaultWebURL,
+    };
+  }
+
+  componentDidMount() {
+    socket.on('event', msg => this.update(msg));
+  }
+
+  update(data) {
+    console.log(data);
+    this.setState({
+      cryptoType: data[0],
+      fiatType: data[1],
+      cryptoAmount: data[2],
+      fiatAmount: data[3],
+      cryptoPrice: data[4],
+      url: data[5],
+    }, () => console.log(this.state));
   }
 
   render() {
-    return(
-      <div className="customer-page">
+    return (
+      <div className="cashier-page">
         <Helmet>
           <title>Customer Page</title>
           <meta
@@ -26,9 +51,12 @@ export default class Customer extends React.Component {
           />
         </Helmet>
         <h4>
-          <b>Login</b> into customer page is {" "}
+          <b>Login</b> into cashier page is {" "}
           <span style={{ fontFamily: "monospace" }}>successful</span>. Made by Bitcoin Bay
         </h4>
+        <p>$ {this.state.cryptoPrice} {this.state.fiatType} / {this.state.cryptoType}</p>
+        <p>{this.state.cryptoAmount} {this.state.cryptoType}</p>
+        <p>$ {this.state.fiatAmount} {this.state.fiatType}</p>
       </div>
     );
   }
