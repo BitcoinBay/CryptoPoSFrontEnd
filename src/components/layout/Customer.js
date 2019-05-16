7792729407d86001646f7becd3cb65161ed1c8a3
ReemaGowda <reemagowdan@gmail.com>
2019-05-16 00:07:56 -0400

    customer page added few feature

--- src/components/layout/Customer.js
+++ src/components/layout/Customer.js
@@ -2,19 +2,64 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import axios from 'axios';
 import openSocket from 'socket.io-client';
-
+import './styles/customer.css'
+import  QRAddress21 from './../QRAddress21';
+import { Dropdown } from 'semantic-ui-react'
+// var QRCode = require('qrcode.react');
 //import * as BITBOXCli from "bitbox-sdk";
 
 // initialize BITBOX
 //const BITBOX = new BITBOXCli.default({ restURL: "https://trest.bitcoin.com/v2/" });
 
 const socket = openSocket('http://localhost:3000');
+const defaultWebURL = 'https://www.meetup.com/The-Bitcoin-Bay';
+
+
+
+const options = [
+  { key: 1, text: 'CAD', value: 1 },
+  { key: 2, text: 'BCH', value: 2 },
+  
+]
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
 export default class Customer extends React.Component {
   constructor() {
     super();
+    this.state = {
+      // value: '45.98',
+      // url: defaultWebURL,
+      cryptoType: 'BCH',
+      fiatType: 'CAD',
+      cryptoAmount: 0,
+      fiatAmount: 0,
+      cryptoPrice: 0,
+      url: defaultWebURL,
+    }
+
   }
 
+  componentDidMount() {
+    socket.on('event', msg => this.update(msg));
+  }
+
+  update(data) {
+    console.log(data);
+    this.setState({
+      cryptoType: data[0],
+      fiatType: data[1],
+      cryptoAmount: data[2],
+      fiatAmount: data[3],
+      cryptoPrice: data[4],
+      url: data[5],
+    }, () => console.log(this.state));
+  }
+  
+  
+  
   render() {
     return(
       <div className="customer-page">
@@ -29,7 +74,32 @@ export default class Customer extends React.Component {
           <b>Login</b> into customer page is {" "}
           <span style={{ fontFamily: "monospace" }}>successful</span>. Made by Bitcoin Bay
         </h4>
-      </div>
+        {/* <h1><QRCode value="http://facebook.github.io/react/" /></h1> */}
+
+        <h3 className="heading">Please Send Your Bitcoin  To This Address</h3>
+      <article>
+      <Helmet>
+        <title>Customer POS Page</title>
+        <meta name="description" content="CashierPOS Page" />
+      </Helmet>
+      { this.state.url === ''
+        ? <QRAddress21 value={defaultWebURL} />
+        : (
+          <div>
+            <QRAddress21 value={this.state.url} />
+          </div>
+        )
+      }
+      <label>Equivalet in CAD</label>
+      <p>$ {this.state.cryptoPrice} {this.state.fiatType} / {this.state.cryptoType}</p>
+      <label>Denominated in</label>
+      <p>{this.state.cryptoAmount} {this.state.cryptoType}</p>
+      <p>$ {this.state.fiatAmount} {this.state.fiatType}</p>
+    </article>
+
+    <Dropdown selection options={options} placeholder='CAD' />
+    
+    </div>
     );
   }
 }
