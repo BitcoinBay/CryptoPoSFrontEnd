bc41e7199c3e8333a869397f9e44cac4e54b5af6
Lars Kloosterman <lars_kloosterman@outlook.com>
2019-05-23 12:50:11 -0400

    Add saving of PoS'

--- src/components/create-pos/CreatePoS.js
+++ src/components/create-pos/CreatePoS.js
@@ -1,32 +1,81 @@
-import React, { Component } from "react"; import PropTypes from "prop-types";
+import React, { Component } from "react";
+import axios from "axios";
 import { connect } from "react-redux";
-import M from "materialize-css"
 
 class CreatePoS extends Component {
 
-    componentDidMount() {
-        M.FormSelect.init(this.select);
+    constructor() {
+        super();
+
+        this.state = {
+            pos_name: "",
+            pos_xpub: ""
+        };
+    }
+
+    onChange = (event) => {
+        this.setState({ [event.target.id]: event.target.value });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        const xpub_data = {
+            address: this.state.pos_xpub
+        };
+
+        axios.post("/api/add-xpub", xpub_data).then((res) => {
+            const pos_data = {
+                user_id: this.props.auth.user.id,
+                name: this.state.pos_name,
+                xpub: res.data._id
+            };
+
+            axios.post("/api/add-pos", pos_data).then((res) => {
+
+            });
+        });
     }
 
     render() {
         return(
             <div className="container">
                 <div className="row" style={{ marginTop: "4rem" }}>
-                    <div className="col s6 offset-s3">
-                        <h4 className="page-header"
-                                style={{ paddingLeft: "11px" }}>
-                            Create a New Point-of-Sale
-                        </h4>
+                    <div className="col s8 offset-s2">
+                        <div className="col s12">
+                            <h4 className="page-header">
+                                Create a New Point-of-Sale
+                            </h4>
+                        </div>
 
-                        <form novalidate>
+                        <form novalidate onSubmit={this.onSubmit}>
                             <div className="input-field col s12">
-                                <input id="pos_name" type="text"/>
+                                <input id="pos_name" type="text"
+                                        onChange={this.onChange}
+                                        value={this.state.value}/>
                                 <label htmlFor="pos_name">Point-of-sale name</label>
                             </div>
 
                             <div className="input-field col s12">
-                                <input id="pos_xpub" type="text"/>
-                                <label htmlFor="pos_xpub">Wallet xPub Address</label>
+                                <input id="pos_xpub" type="text"
+                                        onChange={this.onChange}
+                                        value={this.state.value}/>
+                                <label htmlFor="pos_xpub">
+                                    Wallet xPub Address
+                                </label>
+                            </div>
+                            <div className="col s12"
+                                    style={{ paddingLeft: "11.250px" }}>
+                                <button
+                                style={{
+                                    width: "165px",
+                                    borderRadius: "3px",
+                                    letterSpacing: "1.5px",
+                                    marginTop: "1rem"
+                                }}
+                                type="submit"
+                                className="btn btn-large waves-effect waves-light
+                                        hoverable blue accent-3">Create PoS</button>
                             </div>
                         </form>
                     </div>
@@ -37,12 +86,8 @@ class CreatePoS extends Component {
 
 }
 
-CreatePoS.propTypes = {
-    auth: PropTypes.object.isRequired
-}
-
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
     auth: state.auth
-})
+});
 
 export default connect(mapStateToProps)(CreatePoS);
